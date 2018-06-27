ef2118c4121c10ffd7738fec290a62ba3cfd8a9a
pilatae66 <36156500+pilatae66@users.noreply.github.com>
2018-06-28 04:36:53 +0800

    First Commit

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Student::class, function (Faker $faker) {
+    return [
+        'id' => $faker->bothify('##-####'),
+        'firstname' => $faker->firstName,
+        'lastname' => $faker->lastName,
+        'course' => $faker->randomElement($array = array ('BSIT','BSCS')),
+        'year' => $faker->randomElement($array = array ('1st','2nd','3rd','4th')),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+    ];
+});
---
+++ database/migrations/2018_06_27_201841_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('year');
+            $table->string('course');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
