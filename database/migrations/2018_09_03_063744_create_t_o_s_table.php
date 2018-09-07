c47d2d08ab07fc3549e2767cc0ecc71a3b9aa428
pilatae66 <pilatae66@gmail.com>
2018-09-07 09:56:34 +0800

    test

--- database/migrations/2018_09_03_063744_create_t_o_s_table.php
+++ database/migrations/2018_09_03_063744_create_t_o_s_table.php
@@ -20,6 +20,7 @@ class CreateTOSTable extends Migration
             $table->integer('knowledge');
             $table->integer('understanding');
             $table->integer('application');
+            $table->integer('total_test_items');
             $table->integer('percentage');
             $table->unsignedInteger('exam_id');
             $table->foreign('exam_id')
